9531ee7b9fafec900666550b706db6c1f3eee6aa
LauraMR112 <nathali-03@hotmail.com>
2024-03-01 15:15:54 +0100

    Data prep code ran in astro-nodes

--- Data_tensors.py
+++ Data_tensors.py
@@ -10,17 +10,12 @@ resource.setrlimit(resource.RLIMIT_NOFILE, (high, high))
 
 import math
 import matplotlib.pyplot as plt
-import tensorflow as tf
-
-from tensorflow import keras
-from keras import layers
 from astropy.io import fits
 from astropy.utils.data import get_pkg_data_filename
 import numpy as np
 import os, glob
 from itertools import compress
 from tqdm import tqdm
-import time
 from astropy.coordinates import SkyCoord
 from astropy.wcs import WCS
 from astropy.nddata import Cutout2D
@@ -28,8 +23,14 @@ from astropy import units as u
 import pandas as pd
 
 def cropping(img_band, header_img, filename, npixels = 60):
-    ra = float(filename[filename.find('ra')+3:filename.find('dec')-1])
-    dec = float(filename[filename.find('dec')+4:filename.find('HSC')-1])
+    # Information in file name is split by substring '_'
+    indx1 = filename.find('_')				# At the beginning of an ID number
+    indx2 = filename[indx1 + 1: ].find('_') + indx1+1	# Between ID and ra
+    indx3 = filename[indx2 + 1: ].find('_') + indx2+1	# Between ra and dec
+    indx4 = filename[indx3 + 1: ].find('_') + indx3+1	# At the end of dec
+
+    ra = float(filename[indx2 + 1 : indx3])
+    dec = float(filename[indx3 + 1 : indx4])
     position = SkyCoord(ra, dec, unit = 'deg', frame='icrs')
     wcs = WCS(header_img)
     size = u.Quantity(img_band.shape, u.pixel)
@@ -58,9 +59,9 @@ def bad_pixels(image, name):
         pass
     return image
     
-def load_tensors(path, npixels = 60, np_norm = 10, substring = None):
+def load_tensors(path1, path2, npixels = 60, np_norm = 10, substring = None):
     names = []
-    for file in os.listdir(path):
+    for file in os.listdir(path1):   
         filename = os.fsdecode(file)
         # Substring for labeled dataset
         if substring != None and filename.find(substring) != -1:
@@ -71,6 +72,8 @@ def load_tensors(path, npixels = 60, np_norm = 10, substring = None):
         elif substring == None and filename.find('QSOs') == -1 and \
             filename.find('Gals') == -1 and filename.find('Stars') == -1:
             names.append(filename[:-6])
+        elif filename[-5:] != '.fits':
+            print('Non-fits file:{}'.format(filename))
             
     unique_names = np.unique(names)
     print('Creating a tensor with {} sources. '.format(len(unique_names)))
@@ -78,20 +81,29 @@ def load_tensors(path, npixels = 60, np_norm = 10, substring = None):
     print('Cropping each image from size {} to {} arcsecs.'.format(4.66666666666311E-05*84*3600/2, 4.66666666666311E-05*npixels*3600/2))
     print('Normalizing tensor by the maximum of the i-flux distribution in {} arcsec around the source. '.format(4.66666666666311E-05*np_norm*3600/2))
 
-    all_im_5 = np.zeros((len(unique_names),npixels, npixels, 5))
-    start_time = time.time()
+    #all_im_5 = np.zeros((len(unique_names),npixels, npixels, 5))
+    all_im_5 = []
+    success_files = []
+
     for j in tqdm(range(len(unique_names))):
         bands = ['G', 'R', 'I', 'Z', 'Y']
+        im_5 = []
         for b in range(len(bands)):
-            fits_band = get_pkg_data_filename(path + unique_names[j] + bands[b] + '.fits')
-            img_band = fits.getdata(fits_band, ext = 1)
-            header_img = fits.getheader(fits_band, ext = 1)
-            img_band_bp = bad_pixels(img_band, unique_names[j] + bands[b] + '.fits')
-            img_band_crop = cropping(img_band_bp, header_img,  unique_names[j])
-            all_im_5[j,:,:,b] = img_band_crop
+            with fits.open(path1 + unique_names[j] + bands[b] + '.fits', memmap=False) as hdul:
+                img_band = hdul[1].data
+                header_img = hdul[1].header
+                img_band_bp = bad_pixels(img_band, unique_names[j] + bands[b] + '.fits')
+                img_band_crop = cropping(img_band_bp, header_img,  unique_names[j])
+                im_5.append(img_band_crop)
+                success_files.append(unique_names[j])
+        all_im_5.append(im_5)
+
+    #Change dimension of bands to be the last one
+    all_im_5 =  np.moveaxis(np.array(all_im_5), [1], [3])
     norm_all_im_5 = total_norm(all_im_5, np_norm)
     if substring == None:
-        np.save('train_set.npy', norm_all_im_5)
+        np.save(path2 + 'train_set.npy', norm_all_im_5)
+        np.save(path2 + 'ID_ra_dec.npy', success_files)
     else:
         return norm_all_im_5
 
@@ -110,8 +122,10 @@ def add_labeled_data(path, class_names, npixels = 60, np_norm = 10):
     np.save('labeled_set.npy', labeled_set)
     np.save('labels.npy', labels)
 
-print('Creating and saving tensor of images.......................')
-load_tensors('HSC/', 60, 10) 
-print('Creating and saving tensors of labeled images and labels .......................')
-add_labeled_data('HSC/', ['QSOs', 'Stars', 'Gals'], 60, 10)
+os.chdir('/data/beegfs/astro-storage/groups/banados')
+path = os.getcwd()
+print('Creating and saving tensor of images.......................') 
+load_tensors(path +'/jwolf/hsc_cutouts/', path + '/lmartinez/hsc_tensors/', 60, 10) 
+#print('Creating and saving tensors of labeled images and labels .......................')
+#add_labeled_data('HSC/', ['QSOs', 'Stars', 'Gals'], 60, 10)
 
